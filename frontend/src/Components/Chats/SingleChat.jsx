986f2fce3a056192af4e092d57bac3628b354ea5
yash987 <ys2599518@gmail.com>
2024-10-27 12:10:42 +0530

    final

--- frontend/src/Components/Chats/SingleChat.jsx
+++ frontend/src/Components/Chats/SingleChat.jsx
@@ -22,7 +22,7 @@ import io from 'socket.io-client';
 import Lottie from 'lottie-react';
 import animations from '../animations/lottie.json';
 import { NotificationAtom } from '../../store/notifications';
-const ENDPOINT = `${window.location.origin}`;
+const ENDPOINT = `https://chatapp2-row1.onrender.com/`;
 
 let selectedChatCompare;
 let socket;
